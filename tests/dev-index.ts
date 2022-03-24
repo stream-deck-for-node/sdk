e3ba5bcfeac09f0f73dae2fd11b05eef3a9c31ae
Francesco Saverio Cannizzaro <francescosaveriocannizzaro@gmail.com>
2022-03-24 20:01:53 +0100

    fix tests

--- tests/dev-index.ts
+++ tests/dev-index.ts
@@ -1,16 +1,35 @@
-import "./dev-plugin-mock";
 import { StreamDeck } from "../src";
 import { expect } from "chai";
 import "./stream-deck-mock";
 import { TEST_PLUGIN_ID, TEST_PLUGIN_UUID } from "./stream-deck-mock";
+import { tmpdir } from "os";
+import { writeFileSync } from "fs";
+import { WebSocketServer } from "ws";
 
 process.argv = ["node", "script.js", "-debug", TEST_PLUGIN_ID];
+
+const fileConfig = `${tmpdir()}/${TEST_PLUGIN_ID}.dev.json`;
+
+new WebSocketServer({ port: 62800 });
+
+writeFileSync(fileConfig, JSON.stringify({
+  "port": 62800,
+  "pluginUUID": TEST_PLUGIN_UUID,
+  "registerEvent": "registerPlugin",
+  "info": {
+    "application": {},
+    "colors": {},
+    "devicePixelRatio": 1,
+    "devices": [],
+    "plugin": { "uuid": TEST_PLUGIN_ID, "version": "1.0.0" }
+  }
+}));
 
 export const sd = new StreamDeck();
 
 describe("Checking development initialization", () => {
 
-  it("Verify plugin UUID and plugin info from IPC", () => {
+  it("Verify plugin UUID and plugin info from WS", () => {
     setTimeout(() => {
       expect(sd.uuid).to.equals(TEST_PLUGIN_UUID);
       expect(sd.info).to.be.not.null;
